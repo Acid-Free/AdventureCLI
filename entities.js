bfbe8765a2b0847afa604fb8631ca68c3dc404ed
Sian Caleb <49774633+Acid-Free@users.noreply.github.com>
2023-01-05 11:07:25 +0800

    Create player class

---
+++ entities.js
@@ -0,0 +1,71 @@
+import { mapDimensions } from "./world.js";
+import _ from "lodash-es";
+
+console.log("🧔🏽💀🐾🐍🦴🌲🌳🎄🍅🔥♺");
+
+class Entity {
+  #health;
+  #attack;
+  #defense;
+  #emoji;
+  constructor(health, attack, defense, emoji) {
+    this.#health = health;
+    this.#attack = attack;
+    this.#defense = defense;
+    this.#emoji = emoji;
+  }
+  get getHealth() {
+    return this.#health;
+  }
+  get getAttack() {
+    return this.#attack;
+  }
+  get getDefense() {
+    return this.#defense;
+  }
+  get getEmoji() {
+    return this.#emoji;
+  }
+  set setHealth(offset) {
+    this.#health += offset;
+  }
+  set setAttack(offset) {
+    this.#attack += offset;
+  }
+  set setDefense(offset) {
+    this.#defense += offset;
+  }
+  set setEmoji(emoji) {
+    this.#emoji = emoji;
+  }
+}
+
+class Player extends Entity {
+  #posX;
+  #posY;
+  constructor(health, attack, defense, emoji, posX = 0, posY = 0) {
+    super(health, attack, defense, emoji);
+    this.#posX = posX;
+    this.#posY = posY;
+  }
+  getPosition() {
+    return { posX: this.#posX, posY: this.#posY };
+  }
+
+  // returns true if movement is valid, otherwise false
+  setPosition(xOffset, yOffset) {
+    this.#posX += xOffset;
+    this.#posY += yOffset;
+    if (this.#posX < 0 || this.#posX > mapDimensions.width) {
+      this.#posX = _.clamp(this.#posX, 0, mapDimensions.width);
+      return false;
+    }
+    if (this.#posY < 0 || this.#posY > mapDimensions.height) {
+      this.#posY = _.clamp(this.#posY, 0, mapDimensions.height);
+      return false;
+    }
+    return true;
+  }
+}
+
+export { Player };
